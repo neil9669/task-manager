57faa335f59f67556a24e199f749271846ccd4b1
neil9669 <palmern69@gmail.com>
2023-09-20 13:43:22 +0000

    final commit

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,7 +12,7 @@ if os.path.exists("env.py"):
     else:
         uri = os.environ.get("DATABASE_URL")
         if uri.startswith("postgres://"):
-            uri = uri.replace("postgres://", "postgresql://", 1)
+            uri = uri.replace("postgres://", "postgres://", 1)
             app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
     db = SQLAlchemy(app)
